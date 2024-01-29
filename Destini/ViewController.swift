49e6a53ce7478e303491dcbf1c3692002e2a530a
Korede612 <korede.oko-osi@nytengaletech.com>
2024-01-29 03:24:09 +0100

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Oko-osi Korede on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
